330310cfcc342580bcdcfca0d7be6006369536a3
furyohfury <artemkuzminikh@gmail.com>
2026-04-20 13:50:24 +0500

    volumeslidercanvas

---
+++ Assets/Game/Scripts/DontDestroyOnLoadObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Game
+{
+    public sealed class DontDestroyOnLoadObject : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
